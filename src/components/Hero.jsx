af864076bb32432aec3afaad4187b1f90bcf90e2
Natalia-Partykowska <natalia.partykowska@gmail.com>
2025-02-12 17:35:46 +0100

    beginning of CSS styling and HTML and UX/UI proposition

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import image1 from "../png/image1.jpg";
+
+
+function Hero() {
+
+  return (
+    <>
+      <section className='hero-container'>
+        <div>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <div className='button-container'>
+            <button className='button'>Reserve a table</button>
+            <button className='button'>View reservations</button>
+          </div>
+        </div>
+        <div>
+          <img src={image1} alt="" />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Hero;
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Highlights() {
+
+  return (
+    <>
+      <section>
+        
+      </section>
+    </>
+  )
+}
+
+export default Highlights;
